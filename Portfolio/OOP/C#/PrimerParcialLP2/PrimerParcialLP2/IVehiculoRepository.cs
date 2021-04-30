a3f3cad995a99d1ce56b0f01ffb82b1919b84e8a
Nurys <nurys.cardy18@gmail.com>
2021-04-30 00:18:23 -0400

    Add files via upload

---
+++ Portfolio/OOP/C#/PrimerParcialLP2/PrimerParcialLP2/IVehiculoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerParcialLP2
+{
+    public interface IVehiculoRepository
+    {
+        OperationResult Create(Vehiculo vehiculo);
+        OperationResult GetAll(Vehiculo vehiculo);
+        OperationResult Get(Vehiculo vehiculo);
+        OperationResult Update(Vehiculo vehiculo);
+        OperationResult Delete(Vehiculo vehiculo);
+
+        
+    }
+}
